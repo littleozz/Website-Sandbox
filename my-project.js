110d91296cb1374420a331e5bfda60e9d2dbb73e
Ryan Osborn <ryanosborn959@gmail.com>
2023-06-07 15:38:59 -0500

    moves files

--- my-project.js
+++ my-project.js
@@ -1,9 +1,12 @@
 console.log('Java file is loaded!');
 
 
-
-
-
+var chatBox = document.getElementById("chat");
+chatBox.addEventListender("keyup", function (event) {
+    if (event.keyCode != 13) {
+        console.log("logged!");
+    }
+});
 
 function myFunction(){
     console.log("hello");
