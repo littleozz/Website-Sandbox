2b7829f7c4bec6ed47e19da6b70c05e13ff94857
Ryan Osborn <ryanosborn959@gmail.com>
2023-06-07 15:30:56 -0500

    test

--- my-project.js
+++ my-project.js
@@ -4,7 +4,7 @@ console.log('Java file is loaded!');
 chatbox.addEventListener("keyup", postChat(Event) );
 
 function postChat(Event){
-    if (Event.keyCode == 13) {
+    if (Event.keyCode != 13) {
         console.log("Posted chat!")
         return 0;
     }
