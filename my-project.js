c5972dcf7ab4165c9f6ae1c1ce0e9c41a3c99a62
Ryan Osborn <ryanosborn959@gmail.com>
2023-06-07 15:11:29 -0500

    test

--- my-project.js
+++ my-project.js
@@ -1,14 +1,17 @@
 console.log('It works!');
 
 
-chatbox.addEventListener("Keyup", myFunction);
+chatbox.addEventListener("Keyup", postChat (event) );
 
 function postChat(){
-
+    if (event.keyCode == 13) {
+        console.log("Posted chat!")
+    }
 }
 
 
 function myFunction(){
+
     console.log("hello");
     return 0;
 }
