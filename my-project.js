93bef1c68ecdf69f4018024ca2d1f68858a4f4ef
Ryan Osborn <ryanosborn959@gmail.com>
2023-06-07 15:39:39 -0500

    undid

--- my-project.js
+++ my-project.js
@@ -1,12 +1,7 @@
 console.log('Java file is loaded!');
 
 
-var chatBox = document.getElementById("chat");
-chatBox.addEventListender("keyup", function (event) {
-    if (event.keyCode != 13) {
-        console.log("logged!");
-    }
-});
+
 
 function myFunction(){
     console.log("hello");
