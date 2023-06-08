a7d00fd30db2b215d82d1c8e2415d51ef344a902
Ryan Osborn <ryanosborn959@gmail.com>
2023-06-08 13:25:26 -0500

    testing storage of data in txt file

--- my-project.js
+++ my-project.js
@@ -1,10 +1,17 @@
 console.log('Java file is loaded!');
 
+const fs = require("fs");
+
+let fileString = fs.readFileSync("msglog.txt").toString();
+console.log(fileString);
+
+
+
 
 var total = 0;
         
 function myClick(){
-    console.log("You clicked the button!");
+    //console.log("You clicked the button!");
     total += 1;
     document.getElementById('total-text').innerHTML = "Times Clicked: " + total;
 }
