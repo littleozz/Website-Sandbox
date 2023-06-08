c6d3f9b27b404378dd195c83a21dffd01cbce4f6
Ryan Osborn <ryanosborn959@gmail.com>
2023-06-08 13:40:48 -0500

    test

--- my-project.js
+++ my-project.js
@@ -2,7 +2,7 @@ console.log('Java file is loaded!');
 
 const fs = require('fs');
 
-fs.writeFile('output.txt', 'Hello World.', err => {
+fs.writeFile('msglogs.txt', 'Hello World.', err => {
     if (err) console.error(err);
     else console.log('Data written to file successfully.');
 
