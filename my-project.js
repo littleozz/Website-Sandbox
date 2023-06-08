8c7d0cbac98ce5607f73ba41f22014ae0c1152c5
Ryan Osborn <ryanosborn959@gmail.com>
2023-06-08 13:20:27 -0500

    rearranged function and var locations

--- my-project.js
+++ my-project.js
@@ -1,6 +1,12 @@
 console.log('Java file is loaded!');
 
 
-
+var total = 0;
+        
+function myClick(){
+    console.log("You clicked the button!");
+    total += 1;
+    document.getElementById('total-text').innerHTML = "Times Clicked: " + total;
+}
 
 
